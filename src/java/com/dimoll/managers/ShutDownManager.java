daeecf0f1212d51ab7d9a0052ea5352612c8492d
Dima Bondarchuk <dimich408@gmail.com>
2018-10-16 20:55:53 +0300

    change some architecture

--- src/java/com/dimoll/managers/ShutDownManager.java
+++ src/java/com/dimoll/managers/ShutDownManager.java
@@ -1,25 +1,21 @@
-package com.dimoll;
+package com.dimoll.managers;
 
 import java.io.IOException;
 
-public class ShutDownManager {
+class ShutDownManager {
 
-    public static void shutDownSystem() {
+    static void shutDownSystem() {
         String property = System.getProperty("os.name");
         System.out.println(property);
 
         if (property.toLowerCase().contains("win")) {
-            String[] commands = new String[]{"shutdown", "-s"};
-
-            shutDownComputer(commands);
+            startCommand(new String[]{"shutdown", "-s"});
         } else if (property.toLowerCase().contains("nux")) {
-            String[] commands = new String[]{"shutdown", "-p", "now"};
-
-            shutDownComputer(commands);
+            startCommand(new String[]{"shutdown", "-p", "now"});
         }
     }
 
-    private static void shutDownComputer(String [] commands) {
+    private static void startCommand(String [] commands) {
         System.out.println("start process");
         try {
             Runtime.getRuntime().exec(commands);
